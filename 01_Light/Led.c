b6bce89af567ded4712b820cd297ed100a8c4a7b
RainLinXi <1419997980@qq.com>
2025-05-17 20:35:45 +0800

    first commit

---
+++ 01_Light/Led.c
@@ -0,0 +1,38 @@
+#include"Led.h"
+
+void led_init(void)
+{
+//	 GPIO_InitTypeDef gpio_init_struct;
+//	 LED0_GPIO_CLK_ENABLE(); /* LED0 ???? */
+//	 LED1_GPIO_CLK_ENABLE(); /* LED1 ???? */
+//	 gpio_init_struct.Pin = LED0_GPIO_PIN; /* LED0 ?? */
+//	 gpio_init_struct.Mode = GPIO_MODE_OUTPUT_PP; /* ???? */
+//	 gpio_init_struct.Pull = GPIO_PULLUP; /* ?? */
+//	 gpio_init_struct.Speed = GPIO_SPEED_FREQ_HIGH; /* ?? */
+//	 HAL_GPIO_Init(LED0_GPIO_PORT, &gpio_init_struct); /* ??? LED0 ?? */
+//	 gpio_init_struct.Pin = LED1_GPIO_PIN; /* LED1 ?? */
+//	 HAL_GPIO_Init(LED1_GPIO_PORT, &gpio_init_struct); /* ??? LED1 ?? */
+//	 
+//	 LED0(1);
+//	
+//	 LED1(1);
+	
+	GPIO_InitTypeDef gpio_init_struct;
+	LED0_GPIO_CLK_ENABLE();
+	LED1_GPIO_CLK_ENABLE();
+	gpio_init_struct.Pin = LED0_GPIO_PIN;
+	gpio_init_struct.Mode = GPIO_MODE_OUTPUT_PP;
+	gpio_init_struct.Pull = GPIO_PULLUP;
+	gpio_init_struct.Speed = GPIO_SPEED_FREQ_HIGH;
+	
+	HAL_GPIO_Init(LED0_GPIO_PORT, &gpio_init_struct);
+	
+	gpio_init_struct.Pin = LED1_GPIO_PIN;
+	
+	HAL_GPIO_Init(LED1_GPIO_PORT, &gpio_init_struct);
+	
+	LED0(1);
+	
+	LED1(1);
+
+}
